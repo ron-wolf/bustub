b772bb3e008cb47a247f8249dac751766df80cf0
Ron Wolf <ron-wolf@users.noreply.github.com>
2021-10-28 14:33:19 -0400

    [Minor] Stop tracking CMake build dirs

--- cmake-build-debug-ilab/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /common/home/raw175/common/home/raw175/2021-10_DBSImpl_Proj1/cmake-build-debug-ilab/googletest-src/googletest
-# Build directory: /common/home/raw175/common/home/raw175/2021-10_DBSImpl_Proj1/cmake-build-debug-ilab/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug-ilab/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /common/home/raw175/common/home/raw175/2021-10_DBSImpl_Proj1/src
-# Build directory: /common/home/raw175/common/home/raw175/2021-10_DBSImpl_Proj1/cmake-build-debug-ilab/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
