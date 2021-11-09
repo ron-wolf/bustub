1301e3e9acdc4f9ba17862b1f8a8b051ca290b93
Ron Wolf <ron-wolf@users.noreply.github.com>
2021-11-09 13:17:50 -0500

    [Monolith] Implement LRUReplacer
    Implement all of LRUReplacer in one commit. No, this was not all done at
    once. Yes, I forgot to commit as I went along.

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -12,6 +12,7 @@
 
 #include "buffer/lru_replacer.h"
 
+#include <cstdint> // intptr_t
 #include <mutex>   // scoped_lock<...MutexTypes>, defer_lock_t defer_lock
 
 namespace bustub {
@@ -23,12 +24,45 @@ LRUReplacer::LRUReplacer(size_t num_pages)
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+  frame_id_t frame_id_v;
+  {
+    std::scoped_lock lk((exc_lk_));
+    
+    if (queue_.empty()) { return false; }
+    
+    frame_id_v = queue_.front();
+    queue_.pop_front();
+    
+    (void) locs_.erase(frame_id_v);
+  }
+  *frame_id = frame_id_v;
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  std::scoped_lock lk((exc_lk_));
+  
+  if (locs_.find(frame_id) == locs_.end()) { return; }
+  
+  auto frame_pos = locs_[frame_id];
+  (void) queue_.erase(frame_pos);
+  locs_.erase(frame_id);
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  std::scoped_lock lk((exc_lk_));
+  
+  if (locs_.find(frame_id) != locs_.end()) {
+    (void) queue_.erase(locs_[frame_id]);
+  }
+  locs_[frame_id] = queue_.insert(queue_.end(), frame_id);
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() {
+  std::scoped_lock lk((shr_lk_));
+  
+  return queue_.size();
+}
 
 }  // namespace bustub
