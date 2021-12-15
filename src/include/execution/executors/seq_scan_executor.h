16054b26b37a49517f580e2cd17906b7a652b683
Ron Wolf <ron-wolf@users.noreply.github.com>
2021-12-14 20:52:27 -0500

    [Fix] Get seq scan working!!!
    The issue was with my usage of TableIterator. In particular:
    
     - It seems the definitions for prefix vs. postfix increment are
       switched by accident in TableIterator
     - operator->() functioned counter to my expectations
     - In between debugging sessions, I would often misplace the
        statement(s) to advance the TableIterator
    
    Phew, glad that's over with. Onward and upward.

--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -12,10 +12,9 @@
 
 #pragma once
 
-#include <iomanip> // std::quoted(char *)
 #include <vector>
 
-#include "common/logger.h"
+#include "common/macros.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/seq_scan_plan.h"
@@ -34,13 +33,16 @@ class SeqScanExecutor : public AbstractExecutor {
    * @param plan the sequential scan plan to be executed
    */
   SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
-      : AbstractExecutor(exec_ctx),
-        table_data_ {exec_ctx_->GetCatalog()->GetTable(plan->GetTableOid())},
-        in_schema_ {&table_data_->schema_},
-        cur_itr_ {table_data_->table_->Begin(exec_ctx_->GetTransaction())},
-        end_itr_ {table_data_->table_->End()},
-        pred_ {plan->GetPredicate()},
-        out_schema_ {plan->OutputSchema()} {}
+      : AbstractExecutor(exec_ctx)
+      , table_data_ {exec_ctx_->GetCatalog()->GetTable(plan->GetTableOid())}
+      , cur_itr_ {table_data_->table_->Begin(exec_ctx_->GetTransaction())}
+      , end_itr_ {table_data_->table_->End()}
+      , pred_ {plan->GetPredicate()}
+      , in_schema_ {&table_data_->schema_}
+      , out_schema_ {plan->OutputSchema()}
+  {
+    BUSTUB_ASSERT(pred_->GetReturnType() == TypeId::BOOLEAN, "Plan predicates must return true or false");
+  }
 
   /** initializes the sequential scan executor. */
   void Init() override {}
@@ -49,43 +51,33 @@ class SeqScanExecutor : public AbstractExecutor {
    * @return true if tuple was found, false otherwise.
    */
   bool Next(Tuple *tuple) override {
-    if (cur_itr_ == end_itr_) {
-      // We are being called again for some reason; complain and identify table
-      // Also give output schema, to aid in finding location of error
-      std::ostringstream quoted_tabnam; // a sink for the quoted table name
-      quoted_tabnam << std::quoted(table_data_->name_); // stream the quoted table name in
-      LOG_ERROR("Iterator already reached end of table %s (oid=%u) -- %s", quoted_tabnam.str().c_str(), table_data_->oid_, out_schema_->ToString().c_str());
-    }
-    
-    bool found = false;
-    for (; cur_itr_ != end_itr_ && !(found); ++cur_itr_) {
-      const Tuple *cand = TupPtrOf(cur_itr_);
-      if (!pred_ || pred_->Evaluate(cand, in_schema_).GetAs<bool>()) {
-        // We have found a matching tuple!
-        // Collect all the fields into a Tuple corresponding to the output Schema
-        std::vector<Value> fields; fields.reserve( out_schema_->GetColumnCount());
-        for (const Column& out_col : out_schema_->GetColumns()) {
-          fields.push_back(out_col.GetExpr()->Evaluate(cand, out_schema_));
+    bool match = false;
+    while (cur_itr_ != end_itr_) {
+      const Tuple& cand = *cur_itr_;
+      match = (pred_ == nullptr) || pred_->Evaluate(&cand, in_schema_).GetAs<bool>();
+      
+      if (match) {
+        std::vector<Value> fields;
+        fields.reserve(out_schema_->GetColumnCount());
+        for (const Column& col : out_schema_->GetColumns()) {
+          fields.push_back(col.GetExpr()->Evaluate(&cand, out_schema_));
         }
         *tuple = Tuple(fields, out_schema_);
-        found = true;
       }
+      ++cur_itr_;
+      if (match) break;
     }
-    return found; // if true, there may be more; if false, the executor has stopped
+    return match;
   }
 
   const Schema *GetOutputSchema() override { return out_schema_; }
 
  private:
   TableMetadata *table_data_;
-  const Schema *in_schema_;
-  
   TableIterator cur_itr_;
   const TableIterator end_itr_;
-  
   const AbstractExpression *pred_;
+  const Schema *in_schema_;
   const Schema *out_schema_;
-  
-  Tuple *TupPtrOf(TableIterator itr) { return itr.operator->(); }
 };
 }  // namespace bustub
